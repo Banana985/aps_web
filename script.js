5a33d5a53e5caabe3ecafb42b734a110660f88a5
LoftyToTheTenthPower <146829407+LoftyToTheTenthPower@users.noreply.github.com>
2023-10-03 22:08:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function toggletextbox() {
+  let textbox = document.getElementById("my-textbox");
+  if(textbox.style.display === "none") {
+    textbox.style.display = "block";
+  } else {
+    textbox.style.display = "none";
+  }
+}
